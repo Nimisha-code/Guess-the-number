b5d7e78b3e12b15eb152edd962f4d4c9cf243309
Nimisha <nimisha.jhunjhunwala@gmail.com>
2021-05-22 09:11:39 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+let displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+let displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    //   When there is input
+    displayMessage('💣 No Number');
+  } else if (guess === secretNumber) {
+    //   When player wins
+    displayMessage('🎊 Correct Number');
+    displayNumber(secretNumber);
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😕 You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start Guessing...');
+  document.querySelector('.score').textContent = score;
+  displayNumber('?');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
